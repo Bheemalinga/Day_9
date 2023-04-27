cc1ae5d2807df8625fc22c451d0261dc984e8344
Bheema_Linga_Sai <95648902+Bheemalinga@users.noreply.github.com>
2023-04-27 16:31:32 +0530

    Add files via upload
    SQL Training

---
+++ HeroVired.sql
@@ -0,0 +1,26 @@
+-- Database - HeroVired
+
+-- Table - RGM
+-- Columns - Name, College, Session, Contact Number, Address
+
+-- 10 entries........
+
+
+
+CREATE DATABASE HeroVired;
+
+CREATE TABLE RGM(
+Name_s VARCHAR(30) NOT NULL,
+College VARCHAR(30) NOT NULL,
+Session_ INT NOT NULL,
+Contact_Number INT NOT NULL,
+Address VARCHAR(40) NOT NULL
+);
+
+INSERT INTO HeroVired Values
+
+
+
+
+
+
